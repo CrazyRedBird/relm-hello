55f981767beafd11c8a5c8b4e01deddf7de214ac
CrazyRedBird <kyoukinoakaitori@foxmail.com>
2018-09-23 02:26:50 +0800

    Update sources and readme file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # relm-hello
 A hello world program using [relm](https://github.com/antoyo/relm).
+
+## Requirements
+This package can only be compiled by a nightly ``rustc``.
---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use {
+    self::Msg::Quit,
+    failure::{format_err, Fallible},
+    gtk::prelude::*,
+    relm::{connect, connect_stream, Widget},
+    relm_attributes::widget,
+    relm_derive::Msg,
+};
+
+#[derive(Msg)]
+pub enum Msg {
+    Quit,
+}
+
+#[widget]
+impl Widget for Win {
+    fn model() -> () {}
+
+    fn update(&mut self, msg: Msg) {
+        match msg {
+            Quit => gtk::main_quit(),
+        }
+    }
+
+    view! {
+        gtk::Window {
+            property_width_request: 300,
+            resizable: false,
+            title: "Relm Hello",
+
+            gtk::Box {
+                orientation: gtk::Orientation::Vertical,
+
+                gtk::Label {
+                    text: "Hello, world!",
+                },
+                gtk::Button {
+                    clicked => Quit,
+                    label: "Close",
+                },
+            },
+
+            delete_event(_, _) => (Quit, Inhibit(false)),
+        }
+    }
+}
+
+fn main() -> Fallible<()> {
+    Win::run(()).map_err(|_| format_err!("relm initialization error"))
+}
